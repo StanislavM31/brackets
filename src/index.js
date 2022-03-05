23945ba6753e16b8af40b6ec4e316fef1d442a4a
Stanislav Minko <minkostas@gmail.com>
2022-03-05 23:22:43 +0300

    feat: object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+  const OPEN_BRACKETS =['(', '{'];
+  const BRACKETS_PAIR = {
+    [')'] : '(',
+    ['}'] : '{',
+    ['['] : ']',
+  };
+
+  function loolingForPair(str){
+    let arr = [];
+    let z = str.length;
+
+    for (let i=0; i < z; i++){
+      let curSimbol = str[i];
+
+      if(OPEN_BRACKETS.includes(curSimbol)) {
+        arr.push(curSimbol);
+      } else {
+        if (z === 0) {
+          return false;
+        }
+      }
+       let topElement = arr[z - 1];
+
+       if (BRACKETS_PAIR[curSimbol] === topElement) {
+         arr.pop();
+       } else {
+         return false;
+       }
+    }
+  }
+
 }
